f33d1d72fac145fd3bbb95eaf5791fb5de5a1e35
Beta-Verse-Hub <160262238+Beta-Verse-Hub@users.noreply.github.com>
2024-05-31 11:47:52 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+document.getElementById('AccountsButton').addEventListener("click", () => {
+    document.getElementById('AccountsSection').scrollIntoView();
+});
+
+document.getElementById('HomeButton').addEventListener("click", () => {
+    document.getElementById('HomeSection').scrollIntoView();
+});
